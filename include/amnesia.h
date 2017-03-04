580cd4e67f348793c8be94b8ac93275811cde43c
Nyxxie <nyxxxxie@gmail.com>
2017-03-03 23:23:37 -0600

    Created lightly-spec'd filler for the library.

---
+++ include/amnesia.h
@@ -0,0 +1,54 @@
+/*****************************************************************************\
+Copyright (C) 2016-2017 Nyxxie <github.com/nyxxxie>
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA  02110-1301, USA.
+\*****************************************************************************/
+
+#ifndef AMNESIA_H
+#define AMNESIA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct amnesia_ctx_t amnesia_ctx_t;
+
+typedef enum {
+    AMNESIA_MODE_PTRACE = 0,
+    AMNESIA_MODE_SYSCALL,
+} amnesia_mode_t;
+
+/* Init/free functions */
+amnesia_ctx_t* amnesia_init(amnesia_ctx_t* ctx, int pid, amnesia_mode_t mode);
+void amnesia_free(amnesia_ctx_t* ctx);
+
+/* Introspective functions */
+amnesia_mode_t amnesia_get_mode(amnesia_ctx_t* ctx);
+int amnesia_get_pid(amnesia_ctx_t* ctx);
+
+/* Read/write primitives */
+int amnesia_read(amnesia_ctx_t* ctx, uintptr_t* ptr, uint8_t** data, size_t* data_len);
+int amnesia_write(amnesia_ctx_t* ctx, uintptr_t* ptr, uint8_t* data, size_t data_len);
+
+/* Process utils */
+int amnesia_get_files();
+int amnesia_get_file_start();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //AMNESIA_H
---
+++ src/amnesia.c
@@ -0,0 +1,63 @@
+#include "amnesia.h"
+
+struct amnesia_ctx_t {
+    int pid;
+    amnesia_mode_t mode;
+};
+
+int open_process(amnesia_ctx_t* ctx, int pid, amnesia_mode_t mode)
+{
+
+}
+
+/**
+ * Initializes an amnesia ctx.
+ *
+ * @param ctx amnesia_ctx_t context to operate on.
+ * @param pid Process id.
+ * @param mode Mode to use to perform memory operations.
+ * @return Pointer to an amnesia_ctx_t context.
+ */
+amnesia_ctx_t* amnesia_init(amnesia_ctx_t* ctx, int pid, amnesia_mode_t mode)
+{
+    return NULL;
+}
+
+/**
+ * Read data to the remote process.
+ *
+ * @param ctx amnesia_ctx_t context to operate on.
+ * @param address Address to read from.
+ * @param amt Amount of data to read
+ * @param data Data that was read from process.
+ * @param data_len Amount of data that was read.
+ * @return Returns value < 0 on error, otherwise 0 on success.
+ */
+int amnesia_read(amnesia_ctx_t* ctx, uintptr_t* address, size_t amt, uint8_t** data, size_t* data_len)
+{
+    return -1;
+}
+
+/**
+ * Writes data to the remote process.
+ *
+ * @param ctx amnesia_ctx_t context to operate on.
+ * @param address Address to write to.
+ * @param data Data to write to the given address.
+ * @param data_len Amount of data to write.
+ * @return Returns value < 0 on error, otherwise 0 on success.
+ */
+int amnesia_write(amnesia_ctx_t* ctx, uintptr_t* address, uint8_t* data, size_t data_len)
+{
+    return -1;
+}
+
+/**
+ * Closes an amnesia ctx.
+ *
+ * @param ctx amnesia_ctx_t to clean up.
+ */
+void amnesia_free(amnesia_ctx_t* ctx)
+{
+
+}
